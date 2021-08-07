8d666ed86e4f00f9edfbc0fe2961759c1e10557c
Vinicius Bohrer <vbsanttos@gmail.com>
2021-08-06 21:26:53 -0300

    typescript parcel

---
+++ typescript-react-parcel/README.md
@@ -0,0 +1,11 @@
+# React with Parcel and Typescript
+
+```bash
+npm run start
+```
+
+---
+
+```bash
+npm run build && cd dist/ && npx serve
+```
---
+++ typescript-react-parcel/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => (
+  <div>
+    <h1>React with Parcel and Typescript</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
